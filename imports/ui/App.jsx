17183753e88bbc21721fe693046d20d8845e7309
Nazar-bnm <nazar.bnm@gmail.com>
2016-06-14 23:03:44 +0300

    five lessons

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,19 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import { createContainer } from 'meteor/react-meteor-data';
+
+import { Tasks } from '../api/tasks.js';
 
 import Task from './Task.jsx';
 
 // App component - represents the whole app
-export default class App extends Component {
-  getTasks() {
-    return [
-      { _id: 1, text: 'This is task 1' },
-      { _id: 2, text: 'This is task 2' },
-      { _id: 3, text: 'This is task 3' },
-    ];
-  }
+//колись було так var App = React.createClass({...})
 
+
+class App extends Component {
+
+    handleSubmit(event) {
+     event.preventDefault();
+
+     // Find the text field via the React ref
+    //  тут записується текст з інпуту
+     const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+// записуєм в базу в колекцію tasks
+     Tasks.insert({
+       text,
+       createdAt: new Date(), // current time
+     });
+
+     // Clear form
+     ReactDOM.findDOMNode(this.refs.textInput).value = '';
+   }
+
+
+  // this.props.tasks -> приходить з ../api/tasks.js   вроді :)
   renderTasks() {
-    return this.getTasks().map((task) => (
+    return this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -25,6 +44,18 @@ export default class App extends Component {
           <h1>Todo List</h1>
         </header>
 
+{/* handleSubmit -  In React, this is how you listen to browser events,
+  like the submit event on the form
+  bind - робить привязку контексту - привязує обробник до події
+  */}
+        <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+          <input
+            type="text"
+            ref="textInput"
+            placeholder="Type to add new tasks"
+          />
+        </form>
+
         <ul>
           {this.renderTasks()}
         </ul>
@@ -32,3 +63,15 @@ export default class App extends Component {
     );
   }
 }
+
+// check validate
+App.propTypes = {
+  tasks: PropTypes.array.isRequired,
+};
+ //  Наскільки я зрозумів тут автоматично слідкується за оновленням
+ // в базі діних і оновлює сторінку і сортує
+export default createContainer(() => {
+  return {
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+  };
+}, App);
